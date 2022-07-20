aea73a59e3a0466f35581cf8a11f16c204c2aceb
Neeraj Lagwankar <neeraj@codebuddy.co>
2022-07-20 12:10:21 +0530

    :sparkles: feat(custom): initial setup

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import { Button } from '@mantine/core';
+
+const App = () => <Button>Hi</Button>;
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MantineProvider } from '@mantine/core';
+import App from './App';
+import './index.scss';
+
+/**
+ * To use tailwindcss with Mantine, we need to set the emotionOptions as given below
+ * https://github.com/mantinedev/mantine/issues/823#issuecomment-1033762396
+ * https://mantine.dev/theming/mantine-provider/#configure-emotion
+ */
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MantineProvider
+      emotionOptions={{ key: 'mantine', prepend: false }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <App />
+    </MantineProvider>
+  </React.StrictMode>,
+);
