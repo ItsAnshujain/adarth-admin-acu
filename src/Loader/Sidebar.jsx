3a9dc521690851c961a413e5e62d8fdb432163c7
Taslim Raza Ansari <taslim@codebuddy.co>
2022-08-29 15:53:26 +0530

    :bug: fix(custom): sidebar width fix

--- src/Loader/Sidebar.jsx
+++ src/Loader/Sidebar.jsx
@@ -4,7 +4,7 @@ const sidebarText = new Array(10).fill(false);
 const height = 24;
 
 const Sidebar = () => (
-  <div className="hidden lg:block lg:col-span-2 mt-4 border border-r border-slate-400">
+  <div className="hidden lg:block lg:col-span-2 mt-4 border border-r border-slate-400 w-[16.66%] h-full">
     <div className="flex flex-col items-start gap-4">
       {sidebarText.map(_ => (
         <div className="ml-4 flex w-full">
