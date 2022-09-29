967fbfc87067f59b5792e74641914c4f55fdd2b4
Arup Dey <arup@codebuddy.co>
2022-09-29 06:23:02 +0000

    🐛 fix(custom): login-logout issue

--- src/hooks/auth.hooks.js
+++ src/hooks/auth.hooks.js
@@ -1,17 +1,55 @@
+import { showNotification } from '@mantine/notifications';
 import { useMutation } from '@tanstack/react-query';
 import { forgotPassword, login, resetPassword } from '../requests/auth.requests';
 
 export const useLogin = () =>
-  useMutation(async data => {
-    const res = await login(data);
-    return res;
-  });
+  useMutation(
+    async data => {
+      const res = await login(data);
+      return res;
+    },
+    {
+      onSuccess: () => {
+        showNotification({
+          title: 'Login',
+          message: 'Logged in successfully',
+          autoClose: 3000,
+          color: 'green',
+        });
+      },
+      onError: err => {
+        showNotification({
+          title: 'Error',
+          message: err?.message,
+          autoClose: 3000,
+          color: 'red',
+        });
+      },
+    },
+  );
 
 export const useForgotPassword = () =>
-  useMutation(async data => {
-    const res = await forgotPassword(data);
-    return res;
-  });
+  useMutation(
+    async data => {
+      const res = await forgotPassword(data);
+      return res;
+    },
+    {
+      onError: err => {
+        showNotification({
+          title: 'Invalid details',
+          message: err.message,
+          color: 'red',
+        });
+      },
+      onSuccess: () => {
+        showNotification({
+          title: 'Request Submitted',
+          message: 'Please check your email for further instructions',
+        });
+      },
+    },
+  );
 
 export const useResetPassword = () =>
   useMutation(async data => {
