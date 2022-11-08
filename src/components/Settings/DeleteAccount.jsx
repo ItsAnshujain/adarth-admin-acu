955c44ccd1210240018bf068a12e4b2fda939b39
rajatcbd <rm2932002@gmail.com>
2022-11-08 12:05:02 +0530

    :recycle: refactor(custom): notification update

--- src/components/Settings/DeleteAccount.jsx
+++ src/components/Settings/DeleteAccount.jsx
@@ -11,12 +11,9 @@ const DeleteAccount = () => {
   return (
     <div className="pl-5 pr-7 mt-4">
       <p className="font-bold text-xl">Are you sure you want to delete your account?</p>
-      <p className="font-medium text-slate-400 text-sm mt-1 mb-3">
-        Lorem ipsum dolor sit inventore veritatis corrupti suscipit!
-      </p>
       <Button
         type="button"
-        className="py-2 px-3 rounded bg-purple-450 text-white mt-1"
+        className="py-2 px-3 rounded bg-purple-450 text-white mt-3"
         onClick={handleAccountDelete}
         loading={isLoading}
       >
