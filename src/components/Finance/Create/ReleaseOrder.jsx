e66606b60a0af8b9d4c98772225313416b0f8200
Arup <arup@codebuddy.co>
2023-01-24 17:50:56 +0530

    :bug: fix(custom): merge fixes

--- src/components/Finance/Create/ReleaseOrder.jsx
+++ src/components/Finance/Create/ReleaseOrder.jsx
@@ -14,7 +14,7 @@ import NumberInput from '../../shared/NumberInput';
 import { useUploadFile } from '../../../hooks/upload.hooks';
 import modalConfig from '../../../utils/modalConfig';
 import NoData from '../../shared/NoData';
-import { supportedType } from '../../../utils';
+import { supportedTypes } from '../../../utils';
 
 const DATE_FORMAT = 'DD MMM YYYY';
 
@@ -266,7 +266,7 @@ const ReleaseOrder = ({ spacesList, totalPrice }) => {
       <div className="border-b">
         <p className="font-semibold text-lg pt-4 pl-5 mb-2">Signature and Stamp</p>
         <div className="pl-5 mb-3">
-          {supportedType.map(item => (
+          {supportedTypes.map(item => (
             <Badge key={item} className="mr-2">
               {item}
             </Badge>
