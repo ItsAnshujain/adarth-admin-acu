18e4828dcf487fe016a87bb5c5bf0e0a3a7eb8f2
Arup <arup@codebuddy.co>
2023-03-30 18:25:51 +0530

    :bug: fix(custom): sidebar ui change

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import SidebarContent from './SidebarContent';
 
 const Sidebar = () => (
-  <div className="hidden lg:block lg:col-span-2 mt-4">
+  <div className="hidden lg:block lg:col-span-2 pt-4 bg-purple-450">
     <SidebarContent className="gap-3 px-5" />
   </div>
 );
