283f299fd7f4d3506ecb7a01e344c0ea8e4bde6e
Arup <arup@codebuddy.co>
2023-06-29 19:50:42 +0530

    :sparkles: feat(custom): add payment info modal and table

--- src/pages/BookingsPage/BookingDetailsPage.jsx
+++ src/pages/BookingsPage/BookingDetailsPage.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import Header from '../../components/modules/bookings/ViewOrders/Header';
 import Overview from '../../components/modules/bookings/ViewOrders/Overview';
 import OrderInformation from '../../components/modules/bookings/ViewOrders/OrderInformation';
@@ -7,37 +6,22 @@ import ProcessPipeline from '../../components/modules/bookings/ViewOrders/Proces
 import { useBookingById, useBookingStats } from '../../apis/queries/booking.queries';
 
 const BookingDetailsPage = () => {
-  const [pageNumber, setPageNumber] = useState(0);
+  const [searchParams] = useSearchParams({ tab: 'order-information' });
+  const tab = searchParams.get('tab');
   const { id } = useParams();
-
   const { data: bookingData, isLoading } = useBookingById(id, !!id);
-  const { data: bookingStats } = useBookingStats('');
-  const [updatedBookingData, setUpdatedBookingData] = useState();
-
-  useEffect(() => {
-    setUpdatedBookingData(bookingData);
-  }, [bookingData]);
+  useBookingStats('');
 
   return (
     <div className="col-span-12 md:col-span-12 lg:col-span-10 border-l border-gray-450 overflow-y-auto px-5">
-      <Header
-        pageNumber={pageNumber}
-        setPageNumber={setPageNumber}
-        bookingId={id}
-        bookingData={bookingData}
-      />
-      {pageNumber === 0 ? (
-        <OrderInformation
-          bookingData={bookingData}
-          isLoading={isLoading}
-          bookingStats={bookingStats}
-          bookingId={id}
-        />
-      ) : pageNumber === 1 ? (
-        <ProcessPipeline bookingData={updatedBookingData} />
-      ) : (
+      <Header bookingId={id} bookingData={bookingData} />
+      {tab === 'order-information' ? (
+        <OrderInformation isLoading={isLoading} />
+      ) : tab === 'process-pipeline' ? (
+        <ProcessPipeline bookingData={bookingData} />
+      ) : tab === 'overview' ? (
         <Overview bookingData={bookingData} isLoading={isLoading} />
-      )}
+      ) : null}
     </div>
   );
 };
