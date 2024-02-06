5422fb1a230f1badba8dec942c40e94d267fd6a4
Abhishek Kakad <abhishekkakad@codebuddy.co>
2024-02-06 17:08:32 +0530

    :bug: fix(custom): resolves restore version issue

--- src/components/modules/proposals/ViewProposal/VersionsDrawer.jsx
+++ src/components/modules/proposals/ViewProposal/VersionsDrawer.jsx
@@ -60,10 +60,10 @@ const VersionsDrawer = ({
     toggleShareOptions(id);
   };
 
-  const restoreProposal = id => {
+  const restoreProposal = (id, parentPropId) => {
     modals.closeModal('restoreProposalModal');
     restoreProposalQuery.mutate(
-      { proposalId, versionId: id },
+      { proposalId: parentPropId, versionId: id },
       {
         onSuccess: res => {
           navigate(`/proposals/view-details/${res.data._id}?${searchParams.toString()}`);
@@ -90,14 +90,14 @@ const VersionsDrawer = ({
     });
   };
 
-  const toggleRestoreProposal = id => {
+  const toggleRestoreProposal = (id, parentPropId) => {
     modals.openModal({
       modalId: 'restoreProposalModal',
       title: 'Restore version',
       children: (
         <ConfirmContent
           description="Are you sure you want to restore?"
-          onConfirm={() => restoreProposal(id)}
+          onConfirm={() => restoreProposal(id, parentPropId)}
           onCancel={() => modals.closeModal('restoreProposalModal')}
           loading={restoreProposalQuery.isLoading}
           classNames="px-8"
@@ -125,58 +125,60 @@ const VersionsDrawer = ({
           <Loader />
         </div>
       ) : proposalVersionsQuery?.data?.docs?.length ? (
-        proposalVersionsQuery?.data?.docs?.map(({ versionTitle, createdAt, _id }, index) => (
-          <div
-            key={_id}
-            className="border border-gray-400 p-2 rounded-md flex flex-col gap-2 my-2 h-fit"
-          >
-            <div className="flex w-full gap-4 justify-between">
-              <Radio
-                label={
-                  <div>
-                    <div className="text-xl truncate" title={`Version ${versionTitle}`}>
-                      Version {versionTitle}
+        proposalVersionsQuery?.data?.docs?.map(
+          ({ versionTitle, createdAt, _id, parentProposalId }, index) => (
+            <div
+              key={_id}
+              className="border border-gray-400 p-2 rounded-md flex flex-col gap-2 my-2 h-fit"
+            >
+              <div className="flex w-full gap-4 justify-between">
+                <Radio
+                  label={
+                    <div>
+                      <div className="text-xl truncate" title={`Version ${versionTitle}`}>
+                        Version {versionTitle}
+                      </div>
+                      <div className="text-sm ">{dayjs(createdAt).format(DATE_FORMAT)}</div>
                     </div>
-                    <div className="text-sm ">{dayjs(createdAt).format(DATE_FORMAT)}</div>
-                  </div>
-                }
-                classNames={{ inner: 'mt-2', root: 'w-1/2', label: 'w-[280px]' }}
-                onClick={() => onVersionClick(_id)}
-                checked={proposalId === _id || parentVersionTitle === versionTitle}
-              />
-              <div className="flex">
-                {index !== 0 ? (
+                  }
+                  classNames={{ inner: 'mt-2', root: 'w-1/2', label: 'w-[280px]' }}
+                  onClick={() => onVersionClick(_id)}
+                  checked={proposalId === _id || parentVersionTitle === versionTitle}
+                />
+                <div className="flex">
+                  {index !== 0 ? (
+                    <Button
+                      size="xs"
+                      title="Restore"
+                      className="text-black px-2"
+                      onClick={() => toggleRestoreProposal(_id, parentProposalId)}
+                    >
+                      <IconHistory size={22} />
+                    </Button>
+                  ) : null}
                   <Button
                     size="xs"
-                    title="Restore"
+                    title="Share"
                     className="text-black px-2"
-                    onClick={() => toggleRestoreProposal(_id)}
+                    onClick={() => shareVersion(_id)}
                   >
-                    <IconHistory size={22} />
+                    <IconShare size={22} />
                   </Button>
-                ) : null}
-                <Button
-                  size="xs"
-                  title="Share"
-                  className="text-black px-2"
-                  onClick={() => shareVersion(_id)}
-                >
-                  <IconShare size={22} />
-                </Button>
-                {index !== 0 ? (
-                  <Button
-                    size="xs"
-                    title="Delete"
-                    className="text-red-350 px-2"
-                    onClick={() => toggleDeleteVersion(_id, versionTitle)}
-                  >
-                    <IconTrash size={22} />
-                  </Button>
-                ) : null}
+                  {index !== 0 ? (
+                    <Button
+                      size="xs"
+                      title="Delete"
+                      className="text-red-350 px-2"
+                      onClick={() => toggleDeleteVersion(_id, versionTitle)}
+                    >
+                      <IconTrash size={22} />
+                    </Button>
+                  ) : null}
+                </div>
               </div>
             </div>
-          </div>
-        ))
+          ),
+        )
       ) : (
         <div className="text-center">No versions available</div>
       )}
