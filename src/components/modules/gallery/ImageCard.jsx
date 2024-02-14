4d6bd69b4514f418a76dc580ebe818490686eea2
Abhishek Kakad <abhishekkakad@codebuddy.co>
2024-02-14 15:25:04 +0530

    :bug: fix(custom): adds delete confirmation modal

--- src/components/modules/gallery/ImageCard.jsx
+++ src/components/modules/gallery/ImageCard.jsx
@@ -1,40 +1,63 @@
 import { ActionIcon, Button, Checkbox, Image, Menu } from '@mantine/core';
+import { useModals } from '@mantine/modals';
 import MenuIcon from '../../Menu';
+import ConfirmContent from '../../shared/ConfirmContent';
+import modalConfig from '../../../utils/modalConfig';
 
-const ImageCard = ({ image }) => (
-  <div className="rounded-lg border border-gray-200">
-    <Image src={image.link} classNames={{ image: 'rounded-t-lg' }} />
-    <div className="p-4 flex justify-between items-center">
-      <Checkbox label="image.png" />
-      <div className="relative">
-        <Menu>
-          <Menu.Target>
-            <ActionIcon>
-              <MenuIcon />
-            </ActionIcon>
-          </Menu.Target>
-          <Menu.Dropdown className="rounded-none -left-20">
-            <div className="flex flex-col gap-1">
-              <Button
-                variant="default"
-                className="border-none text-left font-normal"
-                classNames={{ inner: 'float-left' }}
-              >
-                Copy link
-              </Button>
-              <Button
-                variant="default"
-                className="border-none text-left font-normal"
-                classNames={{ inner: 'float-left' }}
-              >
-                Delete
-              </Button>
-            </div>
-          </Menu.Dropdown>
-        </Menu>
+const ImageCard = ({ image }) => {
+  const modals = useModals();
+  const toggleDelete = () => {
+    modals.openModal({
+      modalId: 'deleteVersionModal',
+      title: 'Delete Image',
+      children: (
+        <ConfirmContent
+          onConfirm={() => {}}
+          onCancel={() => modals.closeModal('deleteVersionModal')}
+          loading={false} // query isLoading
+          classNames="px-6"
+        />
+      ),
+      ...modalConfig,
+      size: 'md',
+    });
+  };
+  return (
+    <div className="rounded-lg border border-gray-200">
+      <Image src={image.link} classNames={{ image: 'rounded-t-lg' }} />
+      <div className="p-4 flex justify-between items-center">
+        <Checkbox label="image.png" />
+        <div className="relative">
+          <Menu closeOnItemClick>
+            <Menu.Target>
+              <ActionIcon>
+                <MenuIcon />
+              </ActionIcon>
+            </Menu.Target>
+            <Menu.Dropdown className="rounded-none -left-20">
+              <div className="flex flex-col gap-1">
+                <Button
+                  variant="default"
+                  className="border-none text-left font-normal"
+                  classNames={{ inner: 'float-left' }}
+                >
+                  Copy link
+                </Button>
+                <Button
+                  variant="default"
+                  className="border-none text-left font-normal"
+                  classNames={{ inner: 'float-left' }}
+                  onClick={toggleDelete}
+                >
+                  Delete
+                </Button>
+              </div>
+            </Menu.Dropdown>
+          </Menu>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ImageCard;
--- src/pages/GalleryPage/GalleryImagesDashboard.jsx
+++ src/pages/GalleryPage/GalleryImagesDashboard.jsx
@@ -1,40 +1,66 @@
 import { Button, Pagination } from '@mantine/core';
 import { IconTrash } from '@tabler/icons';
+import { useModals } from '@mantine/modals';
 import ImagesList from '../../components/modules/gallery/ImagesList';
 import Header from '../../components/modules/gallery/Header';
 import ImagesPerPage from '../../components/modules/gallery/ImagesPerPage';
+import ConfirmContent from '../../components/shared/ConfirmContent';
+import modalConfig from '../../utils/modalConfig';
 
-const GalleryImagesDashboardPage = () => (
-  <div className="col-span-12 md:col-span-12 lg:col-span-10 border-l border-gray-450 overflow-y-auto">
-    <Header />
-    <div className="w-full px-5">
-      <div className="flex items-center gap-3 text-sm text-gray-6 font-medium text-gray-500 justify-between w-full">
-        <div className="flex items-center gap-3">
-          <ImagesPerPage count="10" />
-          <Button variant="default" className="text-purple-450 p-0 border-none">
-            <IconTrash size={24} />
-          </Button>
-          <Button variant="filled" className="bg-black">
-            Copy link
-          </Button>
+const GalleryImagesDashboardPage = () => {
+  const modals = useModals();
+  const toggleDelete = () => {
+    modals.openModal({
+      modalId: 'deleteVersionModal',
+      title: 'Delete Images',
+      children: (
+        <ConfirmContent
+          onConfirm={() => {}}
+          onCancel={() => modals.closeModal('deleteVersionModal')}
+          loading={false} // query isLoading
+          classNames="px-6"
+        />
+      ),
+      ...modalConfig,
+      size: 'md',
+    });
+  };
+  return (
+    <div className="col-span-12 md:col-span-12 lg:col-span-10 border-l border-gray-450 overflow-y-auto">
+      <Header />
+      <div className="w-full px-5">
+        <div className="flex items-center gap-3 text-sm text-gray-6 font-medium text-gray-500 justify-between w-full">
+          <div className="flex items-center gap-3">
+            <ImagesPerPage count="10" />
+            <Button
+              variant="default"
+              className="text-purple-450 p-0 border-none"
+              onClick={toggleDelete}
+            >
+              <IconTrash size={24} />
+            </Button>
+            <Button variant="filled" className="bg-black">
+              Copy link
+            </Button>
+          </div>
+          <div className="flex justify-end my-4">
+            <Pagination
+              styles={theme => ({
+                item: {
+                  color: theme.colors.gray[5],
+                  fontWeight: 700,
+                },
+              })}
+              page={1}
+              onChange={() => {}}
+              total={10}
+            />
+          </div>
         </div>
-        <div className="flex justify-end my-4">
-          <Pagination
-            styles={theme => ({
-              item: {
-                color: theme.colors.gray[5],
-                fontWeight: 700,
-              },
-            })}
-            page={1}
-            onChange={() => {}}
-            total={10}
-          />
-        </div>
+        <ImagesList />
       </div>
-      <ImagesList />
     </div>
-  </div>
-);
+  );
+};
 
 export default GalleryImagesDashboardPage;
