b7bdb505c4b693937d43ff1e51f552824b83a0f9
Abhishek Kakad <abhishekkakad@codebuddy.co>
2024-03-13 16:58:45 +0530

    :bug: fix(custom): resolves lead drawer style issues

--- src/components/modules/leads/LeadsOverview.jsx
+++ src/components/modules/leads/LeadsOverview.jsx
@@ -23,8 +23,8 @@ const LeadsOverview = ({ leadData }) => {
   return (
     <div>
       <div className="flex gap-2">
-        <div className="border border-gray-200 flex items-center text-gray-400 text-sm rounded-md px-2 w-fit">
-          <div className="text-sm">Primary Incharge - </div>
+        <div className="border border-gray-200 flex items-center text-gray-400 text-base rounded-md px-2 w-fit">
+          <div className="text-base">Primary Incharge - </div>
           <Select
             clearable
             searchable
@@ -43,7 +43,7 @@ const LeadsOverview = ({ leadData }) => {
           />
         </div>
         <div className="border border-gray-200 flex items-center text-gray-400 text-md rounded-md px-2 w-fit">
-          <div className="text-sm">Secondary Incharge - </div>
+          <div className="text-base">Secondary Incharge - </div>
           <Select
             clearable
             searchable
@@ -64,61 +64,63 @@ const LeadsOverview = ({ leadData }) => {
       </div>
       <div className="flex flex-col gap-2 py-4">
         <div className="text-xl font-bold">{leadData?.leadCompany?.companyName}</div>
-        <div className="text-sm text-black/80">{leadData?.objective}</div>
+        <div className="text-base text-black/80">{leadData?.objective}</div>
         <div className="text-lg font-bold">Basic information</div>
         <div className="flex">
-          <div className="flex flex-col w-1/4">
-            <div className="text-sm text-gray-500 pb-1">Start Date</div>
-            <div className="text-sm">
+          <div className="flex flex-col w-1/3">
+            <div className="text-base text-gray-500 pb-1">Start Date</div>
+            <div className="text-base">
               {leadData?.targetStartDate
                 ? dayjs(leadData?.targetStartDate).format(DATE_FORMAT)
                 : '-'}
             </div>
           </div>
-          <div className="flex flex-col w-1/4">
-            <div className="text-sm text-gray-500 pb-1">End Date</div>
-            <div className="text-sm">
+          <div className="flex flex-col w-1/3">
+            <div className="text-base text-gray-500 pb-1">End Date</div>
+            <div className="text-base">
               {' '}
               {leadData?.targetEndDate ? dayjs(leadData?.targetEndDate).format(DATE_FORMAT) : '-'}
             </div>
           </div>
         </div>
         <div className="flex">
-          <div className="flex flex-col  w-1/4">
-            <div className="text-sm text-gray-500 pb-1">Contact Person</div>
-            <div className="text-sm">{leadData?.contact?.name}</div>
+          <div className="flex flex-col  w-1/3">
+            <div className="text-base text-gray-500 pb-1">Contact Person</div>
+            <div className="text-base">{leadData?.contact?.name}</div>
           </div>
-          <div className="flex flex-col  w-1/4">
-            <div className="text-sm text-gray-500 pb-1">Contact Number</div>
-            <div className="text-sm">
+          <div className="flex flex-col  w-1/3">
+            <div className="text-base text-gray-500 pb-1">Contact Number</div>
+            <div className="text-base">
               {leadData?.contact?.contactNumber ? `+91${leadData?.contact?.contactNumber}` : '-'}
             </div>
           </div>
         </div>
-        <div className="flex flex-col  w-1/4">
-          <div className="text-sm text-gray-500 pb-1">Display Brand</div>
-          <div className="text-sm">{leadData?.brandDisplay}</div>
+        <div className="flex flex-col  w-1/3">
+          <div className="text-base text-gray-500 pb-1">Display Brand</div>
+          <div className="text-base">{leadData?.brandDisplay || '-'}</div>
         </div>
-        <div className="flex flex-col  w-1/4">
-          <div className="text-sm text-gray-500 pb-1">Company Representing</div>
-          <div className="text-sm">{leadData?.companyRepresenting?.companyName}</div>
+        <div className="flex flex-col  w-1/3">
+          <div className="text-base text-gray-500 pb-1">Company Representing</div>
+          <div className="text-base">{leadData?.companyRepresenting?.companyName || '-'}</div>
         </div>
         <Divider className="my-6" />
         <div className="text-base font-bold">Other information</div>
         <div className="flex">
-          <div className="flex flex-col  w-1/4">
-            <div className="text-sm text-gray-500 pb-1">Overall Budget</div>
-            <div className="text-sm">{indianCurrencyInDecimals(leadData?.overAllBudget || 0)}</div>
+          <div className="flex flex-col  w-1/3">
+            <div className="text-base text-gray-500 pb-1">Overall Budget</div>
+            <div className="text-base">
+              {indianCurrencyInDecimals(leadData?.overAllBudget || 0)}
+            </div>
           </div>
-          <div className="flex flex-col  w-1/4">
-            <div className="text-sm text-gray-500 pb-1">Lead Close Date</div>
-            <div className="text-sm">
+          <div className="flex flex-col  w-1/3">
+            <div className="text-base text-gray-500 pb-1">Lead Close Date</div>
+            <div className="text-base">
               {leadData?.leadCloseDate ? dayjs(leadData?.leadCloseDate).format(DATE_FORMAT) : '-'}
             </div>
           </div>
         </div>
         <div className="flex flex-col">
-          <div className="text-sm text-gray-500 pb-1">Target Audience</div>
+          <div className="text-base text-gray-500 pb-1">Target Audience</div>
           <div className="flex gap-2">
             {leadData?.targetAudience?.[0]?.length
               ? leadData?.targetAudience?.map(target => (
@@ -130,7 +132,7 @@ const LeadsOverview = ({ leadData }) => {
           </div>
         </div>
         <div className="flex flex-col">
-          <div className="text-sm text-gray-500 pb-1">Campaign Theme</div>
+          <div className="text-base text-gray-500 pb-1">Campaign Theme</div>
           {leadData?.campaignTheme ? (
             <Badge size="xs" className="bg-purple-450 text-white w-fit p-3 my-1 capitalize">
               {leadData?.campaignTheme}
@@ -140,7 +142,7 @@ const LeadsOverview = ({ leadData }) => {
           )}
         </div>
         <div className="flex flex-col">
-          <div className="text-sm text-gray-500 pb-1">Brand Competitor</div>
+          <div className="text-base text-gray-500 pb-1">Brand Competitor</div>
 
           <div className="flex gap-2">
             {leadData?.brandCompetitors?.[0]?.length
@@ -153,16 +155,16 @@ const LeadsOverview = ({ leadData }) => {
           </div>
         </div>
         <div className="flex flex-col">
-          <div className="text-sm text-gray-500 pb-1">Prospect</div>
-          <div className="text-sm">{leadData?.prospect || '-'}</div>
+          <div className="text-base text-gray-500 pb-1">Prospect</div>
+          <div className="text-base">{leadData?.prospect || '-'}</div>
         </div>
         <div className="flex flex-col">
-          <div className="text-sm text-gray-500 pb-1">Lead Source</div>
-          <div className="text-sm">{leadData?.leadSource || '-'}</div>
+          <div className="text-base text-gray-500 pb-1">Lead Source</div>
+          <div className="text-base">{leadData?.leadSource || '-'}</div>
         </div>
         <div className="flex flex-col">
-          <div className="text-sm text-gray-500 pb-1">Remarks</div>
-          <div className="text-sm">{leadData?.remarksComments || '-'}</div>
+          <div className="text-base text-gray-500 pb-1">Remarks</div>
+          <div className="text-base">{leadData?.remarksComments || '-'}</div>
         </div>
       </div>
     </div>
