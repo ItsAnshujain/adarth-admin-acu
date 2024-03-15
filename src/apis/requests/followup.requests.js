188ed67b0c7e7200af0171fb791ab7445fa7de30
Abhishek Kakad <abhishekkakad@codebuddy.co>
2024-03-15 16:29:36 +0530

    :sparkles: feat(custom): adds feat of followup edit and delete

--- src/apis/requests/followup.requests.js
+++ src/apis/requests/followup.requests.js
@@ -3,6 +3,10 @@ import http from '../http';
 
 const addFollowUp = (id, payload) => http.post(`/lead/${id}/follow-up`, payload);
 
+export const updateFollowUp = (id, payload) => http.patch(`/lead/follow-up/${id}`, payload);
+
+export const deleteFollowUp = id => http.delete(`/lead/follow-up/${id}`);
+
 export const fetchFollowUps = (id, query) => http.get(urlcat(`/lead/${id}/follow-up`, query));
 
 export default addFollowUp;
--- src/components/modules/leads/LeadHistoryCard.jsx
+++ src/components/modules/leads/LeadHistoryCard.jsx
@@ -1,53 +1,87 @@
 import { Avatar, Badge } from '@mantine/core';
 import dayjs from 'dayjs';
+import { useModals } from '@mantine/modals';
 import {
   DATE_THIRD_FORMAT,
   leadCommunicationTypeOptions,
   leadStageOptions,
 } from '../../../utils/constants';
+import FollowUpMenuPopover from '../../Popovers/FollowUpMenuPopover';
+import modalConfig from '../../../utils/modalConfig';
+import AddFollowUpContent from './AddFollowUpContent';
 
-const LeadHistoryCard = ({ followUpData }) => (
-  <div className="flex flex-col border border-gray-200 rounded-md w-full p-4 gap-3">
-    <Badge className="bg-purple-350 text-white font-medium w-fit capitalize text-sm py-3">
-      {leadStageOptions?.filter(({ value }) => value === followUpData?.stage)?.[0]?.label}
-    </Badge>
-    <div className="text-sm">{followUpData?.notes}</div>
-    <div className="text-purple-450">
-      {followUpData?.communicationType
-        ? `Over the ${
-            leadCommunicationTypeOptions.filter(
-              type => type.value === followUpData?.communicationType,
-            )?.[0]?.label
-          } communication`
-        : null}
-    </div>
-    <div className="flex">
-      <div className="w-1/3">
-        <div className="text-sm text-gray-500">Primary Incharge</div>
-        <div className="flex items-center">
-          <Avatar />
-          <div className="text-sm">{followUpData?.primaryInCharge?.name || 'None'}</div>
+const updatedModalConfig = {
+  ...modalConfig,
+  classNames: {
+    title: 'font-dmSans text-xl px-4',
+    header: 'p-4 border-b border-gray-450',
+    body: 'px-8',
+    close: 'mr-4',
+  },
+  size: 800,
+};
+
+const LeadHistoryCard = ({ followUpData }) => {
+  const modals = useModals();
+  const toggleEditFollowUp = id =>
+    modals.openModal({
+      title: 'Edit Follow Up',
+      modalId: 'editFollowUpModal',
+      children: (
+        <AddFollowUpContent
+          onCancel={() => modals.closeModal('editFollowUpModal')}
+          leadId={id}
+          followUpData={followUpData}
+        />
+      ),
+      ...updatedModalConfig,
+    });
+  return (
+    <div className="flex flex-col border border-gray-200 rounded-md w-full p-4 gap-3">
+      <div className="flex justify-between items-center">
+        <Badge className="bg-purple-350 text-white font-medium w-fit capitalize text-sm py-3">
+          {leadStageOptions?.filter(({ value }) => value === followUpData?.leadStage)?.[0]?.label}
+        </Badge>
+        <FollowUpMenuPopover itemId={followUpData?._id} toggleEditFollowUp={toggleEditFollowUp} />
+      </div>
+      {followUpData?.notes ? <div className="text-sm">{followUpData?.notes}</div> : null}
+      <div className="text-purple-450">
+        {followUpData?.communicationType
+          ? `Over the ${
+              leadCommunicationTypeOptions.filter(
+                type => type.value === followUpData?.communicationType,
+              )?.[0]?.label
+            } communication`
+          : null}
+      </div>
+      <div className="flex">
+        <div className="w-1/3">
+          <div className="text-sm text-gray-500">Primary Incharge</div>
+          <div className="flex items-center">
+            <Avatar />
+            <div className="text-sm">{followUpData?.primaryInCharge?.name || 'None'}</div>
+          </div>
+        </div>
+        <div className="w-1/3">
+          <div className="text-sm text-gray-500">Secondary Incharge</div>
+          <div className="flex items-center">
+            <Avatar />
+            <div className="text-sm">{followUpData?.secondaryInCharge?.name || 'None'}</div>
+          </div>
         </div>
       </div>
       <div className="w-1/3">
-        <div className="text-sm text-gray-500">Secondary Incharge</div>
+        <div className="text-sm text-gray-500">Next Follow Up</div>
         <div className="flex items-center">
-          <Avatar />
-          <div className="text-sm">{followUpData?.secondaryInCharge?.name || 'None'}</div>
+          <div className="text-sm">
+            {followUpData?.nextFollowUpDate
+              ? dayjs(followUpData?.nextFollowUpDate).format(DATE_THIRD_FORMAT)
+              : '-'}
+          </div>
         </div>
       </div>
     </div>
-    <div className="w-1/3">
-      <div className="text-sm text-gray-500">Next Follow Up</div>
-      <div className="flex items-center">
-        <div className="text-sm">
-          {followUpData?.nextFollowUpDate
-            ? dayjs(followUpData?.nextFollowUpDate).format(DATE_THIRD_FORMAT)
-            : '-'}
-        </div>
-      </div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default LeadHistoryCard;
