4b0f9b6d25e12efdd1eb345d6c8617ec33dcece5
Abhishek Kakad <abhishekkakad@codebuddy.co>
2024-03-18 15:07:12 +0530

    :bug: fix(custom): updates endpoints of followup edit/delete api

--- src/apis/requests/followup.requests.js
+++ src/apis/requests/followup.requests.js
@@ -3,9 +3,9 @@ import http from '../http';
 
 const addFollowUp = (id, payload) => http.post(`/lead/${id}/follow-up`, payload);
 
-export const updateFollowUp = (id, payload) => http.patch(`/lead/follow-up/${id}`, payload);
+export const updateFollowUp = (id, payload) => http.patch(`/lead-followup/${id}`, payload);
 
-export const deleteFollowUp = id => http.delete(`/lead/follow-up/${id}`);
+export const deleteFollowUp = id => http.delete(`/lead-followup/${id}`);
 
 export const fetchFollowUps = (id, query) => http.get(urlcat(`/lead/${id}/follow-up`, query));
 
