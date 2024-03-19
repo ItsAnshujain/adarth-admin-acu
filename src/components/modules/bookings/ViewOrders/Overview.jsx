ff9f325780bd2116ccd0fe71dfa36ea8e157b5aa
Abhishek Kakad <abhishekkakad@codebuddy.co>
2024-03-19 19:17:29 +0530

    :bug: fix(custom): resolves carousel controls fix

--- src/components/modules/bookings/ViewOrders/Overview.jsx
+++ src/components/modules/bookings/ViewOrders/Overview.jsx
@@ -122,7 +122,7 @@ const Overview = ({ bookingData = {}, isLoading }) => {
             initialSlide={imgIndex}
             nextControlIcon={<ChevronRight size={40} className="bg-white rounded-full" />}
             previousControlIcon={<ChevronLeft size={40} className="bg-white rounded-full" />}
-            classNames={{ indicator: 'bg-white-200' }}
+            classNames={{ indicator: 'bg-white-200', control: 'border-none' }}
             getEmblaApi={setEmbla}
           >
             {previewSpacesPhotos.length &&
